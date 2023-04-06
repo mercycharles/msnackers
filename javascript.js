c3bc832d50c648d060c854ae0393ec5331a6f075
mercycharles <oakoth@kabarak.ac.ke>
2023-04-06 13:00:29 -0700

    first commit

---
+++ javascript.js
@@ -0,0 +1,11 @@
+//javascript popups
+function show_alert() {
+  alert("Do you wish to purchase my items!");
+}
+/*
+function show_confirm() var=confirm {"press a button"};
+if (r==true) alert("You press OK");
+else alert("You press CANCEL"); */
+/*
+<input type="button"onclick="show_confirm()"button="showconfirm box" />*/
+
